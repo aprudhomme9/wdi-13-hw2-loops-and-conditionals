c94c9b85ec1482edc38b6490fdecc12ee963282b
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-12 18:32:36 -0500

    Commit 3 - Excited kittens answered

--- javascript/app.js
+++ javascript/app.js
@@ -7,3 +7,25 @@ for(i = 1; i <=20; i++) {
 for(i = 0; i <= 200; i += 2) {
 	console.log(i);
 };
+
+for(i = 0; i < 20; i++){
+	console.log("Love me, pet me! HSSSSSS!" );
+};
+
+for(i = 0; i < 20; i+=2) {
+	let int = Math.random();
+	if(int > 0.75) {
+		let message = "...human...why you taking pictures of me?...";
+	}
+	else if(int > 0.5) {
+		message = "...the catnip made me do it...";
+	}
+
+	else {
+		message = "...why does the red dot always get away...";
+	}
+	
+
+	console.log(message);
+
+}
